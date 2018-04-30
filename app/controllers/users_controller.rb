e6e433418e46f16b016b4f1e4ab6de2feb9fa155
KOJI <ec2-user@ip-172-31-14-164.ap-southeast-1.compute.internal>
2018-04-30 10:26:48 +0000

    Make app simple

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  
-  
-  
   def index
     @users = User.all
   end
@@ -15,14 +12,8 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(
-      name: params[:name],
-      email: params[:email],
-      image_name: "default_user.jpg",
-      password: params[:password]
-    )
+    @user = User.new(name: params[:name], email: params[:email])
     if @user.save
-      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -38,13 +29,6 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
-    
-    if params[:image]
-      @user.image_name = "#{@user.id}.jpg"
-      image = params[:image]
-      File.binwrite("public/user_images/#{@user.image_name}", image.read)
-    end
-    
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
@@ -53,26 +37,4 @@ class UsersController < ApplicationController
     end
   end
   
-  def login_form
-  end
-  
-  def login
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      flash[:notice] = "ログインしました"
-      redirect_to("/posts/index")
-    else
-      @error_message = "メールアドレスまたはパスワードが間違っています"
-      @email = params[:email]
-      @password = params[:password]
-      render("users/login_form")
-    end
-  end
-  
-  def logout
-    session[:user_id] = nil
-    flash[:notice] = "ログアウトしました"
-    redirect_to("/login")
-  end    
 end
