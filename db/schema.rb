a507aa137d5c48615e954d8b422d9bb9b62bbc50
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-12-19 20:51:34 +0900

    Make User model secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429213833) do
+ActiveRecord::Schema.define(version: 20181219104719) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180429213833) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
