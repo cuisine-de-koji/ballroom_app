e9ce888f7bf4eb5f801e232786cd91fa42858cbd
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-12-20 21:38:30 +0900

    Implement login with session

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get '/',                       to: 'home#top'
-  get 'about',                   to: 'home#about'
-  get 'signup',                  to: 'users#new'
+  get    '/',                       to: 'home#top'
+  get    'about',                   to: 'home#about'
+  get    'signup',                  to: 'users#new'
+  get    '/login',                  to: 'sessions#new'
+  post   '/login',                  to: 'sessions#create'
+  delete '/logout',                 to: 'sessions#destroy'
   resources :users
   resources :posts
 end
