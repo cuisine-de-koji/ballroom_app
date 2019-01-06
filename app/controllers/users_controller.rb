e9db4c9b80b62b052562a5391c078109a4cd0c54
Koji Yamamuro <iq1ooo.midori@gmail.com>
2019-01-06 18:36:05 +0900

    Add basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -28,9 +29,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    @user.name = params[:name]
-    @user.email = params[:email]
-    if @user.save
+    if @user.update_attributes(user_params_update)
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -40,7 +39,18 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :sex, :age, :password_confirmation)
     end
 
+    def user_params_update
+      params.require(:user).permit(:name, :email, :sex, :age)
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_path
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219104719) do
+ActiveRecord::Schema.define(version: 20181220130001) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20181219104719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "sex"
+    t.integer "age"
   end
 
 end
